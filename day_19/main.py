e649defa74e8edd70d89551a60c59ef59701cb3b
Ashis B. Maharjan <maharjanashis9@gmail.com>
2025-10-14 18:42:08 +0545

    python project turtle race completed

--- day_19/main.py
+++ day_19/main.py
@@ -1,17 +1,47 @@
+import random
 from turtle import Turtle,Screen
 
 screen=Screen()
 screen.setup(width=500,height=400)
+is_race_on=False
 
 colors=["red","green","blue","yellow","purple","orange"]
-screen.textinput(title="Make a bet",prompt="Please choose turtle color: red, green, blue, yellow, purple, orange=>")
+user_bet=screen.textinput(title="Make a bet",prompt="Please choose turtle color: red, green, blue, yellow, purple, orange")
+user_bet=user_bet.lower()
+while True:
+    if user_bet in colors:
+        break
+    else:
+        print('You bet is out of our listed turtles.')
+        user_bet = screen.textinput(title="Make a bet",prompt="Please choose turtle color: red, green, blue, yellow, purple, orange")
+        user_bet = user_bet.lower()
 
-ypos=-150
-for color in colors:
-    kurma=Turtle(shape="turtle")
-    kurma.color(color)
-    kurma.penup()
-    kurma.goto(x=-230,y=ypos)
-    ypos+=50
+
+all_kurmas=[]
+if user_bet:
+    ypos = -150
+    for color in colors:
+        kurma = Turtle(shape="turtle")
+        kurma.color(color)
+        kurma.penup()
+        kurma.goto(x=-230, y=ypos)
+        ypos += 50
+        all_kurmas.append(kurma)
+    is_race_on=True
+
+    while is_race_on:
+        for kurma in all_kurmas:
+            random_distance=random.randint(0,10)
+            kurma.forward(random_distance)
+            # if kurma.pos()[0]>210:
+            if kurma.xcor()>210:
+                if user_bet==kurma.pencolor():
+                    print(f'Your bet {kurma.pencolor()} turtle won the race.')
+                else:
+                    print(f'Your bet {user_bet} turtle lost {kurma.pencolor()} turtle won.')
+                is_race_on=False
+
+
+
 
 screen.exitonclick()
