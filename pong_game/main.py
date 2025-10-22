e7d3577266387e8d9c00436bb9495d230b3b17d2
Ashis B. Maharjan <maharjanashis9@gmail.com>
2025-10-22 13:16:59 +0545

    paddle created

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,11 +1,17 @@
 from turtle import Turtle,Screen
+from paddle import Paddle
 
+#setting up the screen
 screen=Screen()
 screen.setup(800,600)
 screen.bgcolor('black')
+screen.title('Pong')
 
 
 
+# creating the paddle
+paddle=Paddle()
+paddle.CreatePaddle()
 
 
 
