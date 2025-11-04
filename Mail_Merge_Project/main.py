c0c9dd54400ef7fa7d12292f9944fe74b7303c3b
Ashis B. Maharjan <maharjanashis9@gmail.com>
2025-11-04 18:09:11 +0545

    mail mege new way

--- Mail_Merge_Project/main.py
+++ Mail_Merge_Project/main.py
@@ -1,31 +1,21 @@
 #extracting mail format
 with open('./Input/Letters/starting_letter.txt',mode='r') as file:
     letters=file.read()
-    words=letters.split()
+    print(letters)
+
 
 
 #extracting invited names
 with open('./Input/Names/invited_names.txt',mode='r') as file:
-    names=file.read()
-    name_list=names.split()
+    names=file.readlines()
+    for name in names:
+        new_name=name.strip('\n')
+        new_letter = letters.replace('[name]', new_name)
+        print(new_letter)
+        with open(f'./Output/ReadyToSend/mail_to_{new_name}.txt', 'w') as file:
+            file.write(letters)
 
+    
 
-
-positions=[1,9,14]
-
-
-
-for name in name_list:
-    mail = ''
-    words[1]=name+','
-    for word in words:
-        mail += word
-        if words.index(word) in positions:
-           mail+='\n'
-        else:
-            mail+=" "
-
-    with open(f'./Output/ReadyToSend/mail_to_{name}.txt', 'w') as file:
-        file.write(mail)
 
 
---
+++ Mail_Merge_Project/main1.py
@@ -0,0 +1,31 @@
+#extracting mail format
+with open('./Input/Letters/starting_letter.txt',mode='r') as file:
+    letters=file.read()
+    words=letters.split()
+
+
+#extracting invited names
+with open('./Input/Names/invited_names.txt',mode='r') as file:
+    names=file.read()
+    name_list=names.split()
+
+
+
+positions=[1,9,14]
+
+
+
+for name in name_list:
+    mail = ''
+    words[1]=name+','
+    for index,word in enumerate(words):
+        mail += word
+        if index in positions:
+           mail+='\n'
+        else:
+            mail+=" "
+
+    with open(f'./Output/ReadyToSend/mail_to_{name}.txt', 'w') as file:
+        file.write(mail)
+
+
