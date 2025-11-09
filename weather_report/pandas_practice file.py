3e1c3642d67a052c27ed3fabfe77addcea75be22
Ashis B. Maharjan <maharjanashis9@gmail.com>
2025-11-09 18:41:54 +0545

    task done

--- weather_report/pandas_practice file.py
+++ weather_report/pandas_practice file.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 df=pd.read_csv('./squirrel_data.csv')
-pd.set_option('display.max.columns',31)
-pd.set_option('display.max.rows',100)
-df.head(50)
+# pd.set_option('display.max.columns',31)
+# pd.set_option('display.max.rows',100)
+# df.head(50)
 squirrel_color_df=df['Primary Fur Color']
 squirrel_colors=squirrel_color_df.to_list()
 squirrel_dict={}
@@ -14,6 +14,17 @@ for color in squirrel_colors:
             squirrel_dict[color]=1
         else:
             squirrel_dict[color]+=1
+# print(squirrel_dict)
+# squirrel_color=squirrel_dict.keys()
+# count=squirrel_dict.values()
+#
+# dataframe_dict={
+#     'Fur Color':list(squirrel_color),
+#     'Count':list(count)
+#
+# }
+# print(dataframe_dict)
+
 df_color=pd.DataFrame(list(squirrel_dict.items()),columns=['Fur Color','Count'])
 df_color.to_csv('squirrel_color.csv')
 
