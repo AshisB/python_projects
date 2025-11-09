6befdab8abe100cba08b8483c5725ad3da398b22
Ashis B. Maharjan <maharjanashis9@gmail.com>
2025-11-09 13:44:53 +0545

    learning panda and jupyternotebook

--- weather_report/pandas_practice file.py
+++ weather_report/pandas_practice file.py
@@ -1,11 +1,21 @@
-import pandas
+import pandas as pd
 
+df=pd.read_csv('./squirrel_data.csv')
+pd.set_option('display.max.columns',31)
+pd.set_option('display.max.rows',100)
+df.head(50)
+squirrel_color_df=df['Primary Fur Color']
+squirrel_colors=squirrel_color_df.to_list()
+squirrel_dict={}
 
+for color in squirrel_colors:
+    if not pd.isna(color):
+        if color not in squirrel_dict :
+            squirrel_dict[color]=1
+        else:
+            squirrel_dict[color]+=1
+df_color=pd.DataFrame(list(squirrel_dict.items()),columns=['Fur Color','Count'])
+df_color.to_csv('squirrel_color.csv')
 
-data_dictionary={
-    'name':'Ashis',
-    'address':'Khokana',
-    'phone':9861909242
-}
-data=pandas.Series(data_dictionary)
-print(data.to_list)
+# df['Primary Fur Color'].value_counts().reset_index().to_csv('squirrel_color.csv', index=False, header=['Fur Color', 'Count'])
+
---
+++ weather_report/test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df=pd.read_csv('squirrel_data.csv')
+print(df)
