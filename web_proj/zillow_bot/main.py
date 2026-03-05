aabe849c0f465f3908303e22b1f74e6f4f5effda
Ashis B. Maharjan <maharjanashis9@gmail.com>
2026-03-05 15:10:55 +0545

    started zillow bot project

---
+++ web_proj/zillow_bot/main.py
@@ -0,0 +1,16 @@
+#This program helps us to scrape rent data from Zillow clone site 
+# and then populate to spreadsheet
+
+# import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException,NoSuchElementException
+import os
+
+
+chrome_options=webdriver.Ch
+
+
+if __name__=="__main__":
+    pass
+
