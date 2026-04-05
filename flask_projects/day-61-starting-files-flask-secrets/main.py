c47c871602b927cfc7141d31085434161b545c06
Ashis B. Maharjan <maharjanashis9@gmail.com>
2026-04-05 19:24:14 +0545

    form validation using wtform

--- flask_projects/day-61-starting-files-flask-secrets/main.py
+++ flask_projects/day-61-starting-files-flask-secrets/main.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+from flask import Flask, render_template,redirect,url_for,session
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='secret key'
+bootstrap = Bootstrap5(app)
 
+
+class LoginForm(FlaskForm):
+    username=StringField('username',validators=[InputRequired('username required'),Length(min=5,max=10,message='username should be of length 5 to 10')])
+    password=PasswordField('password',validators=[InputRequired('Password required'),Length(min=5,max=10,message='username should be of length 5 to 10')])
+    submit=SubmitField('submit')
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    formobj=LoginForm()
+    if formobj.validate_on_submit():
+        username_data=formobj.username.data
+        session['username']=username_data
+        return redirect(url_for('dashboard'))
+    return render_template('login.html',form=formobj)
+
+
+@app.route("/dashboard",methods=['GET'])
+def dashboard():
+
+    if 'username' in session:
+        return render_template('success.html',username=session.get('username'))
+    else:
+        return render_template('login.html')
 
 
 if __name__ == '__main__':
